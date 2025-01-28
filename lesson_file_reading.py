c55ca8f08a443c3079e24a32ff15d574a6821d4d
Dima <dimoncik034@gmail.com>
2025-01-28 16:57:25 +0300

    lesson_3

---
+++ lesson_file_reading.py
@@ -0,0 +1,33 @@
+# Чтение файла
+# !!! Можно открыть только существующий файл!!!
+
+# чтение содержимого файла в режиме чтения
+myfile = open("myfile.txt", "r", encoding="utf-8")
+
+# Чтение содержимого файлав переменную
+text = myfile.read()
+
+# закрытие файла после всех операций с ним
+myfile.close()
+
+# print(text)
+# print(text.splitlines())
+# print(len(text.splitlines()))
+#
+# new_file = open("new_file.txt", "w", encoding="utf-8")
+# text = "I Love Python!"
+# new_file.write(text+'\n')
+# new_file.write("Python forever!!!")
+#
+# names = ["Masha", "Sasha", "Ivan"]
+# for word in names:
+#     new_file.write(word+'\n')
+#
+# names_string = "\n".join(names)
+# new_file.write(names_string+'\n')
+#
+# new_file.close()
+
+new_file = open("new_file.txt", "w", encoding="utf-8")
+new_file.write("\nNEW TEXT")
+new_file.close()
